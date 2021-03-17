5599f943dc646feeb09525f951ca728c3c90bfb3
jaehook293 <78770230+jaehook293@users.noreply.github.com>
2021-03-17 15:37:42 +0900

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+var newContent = prompt("자바스크립트는 HTML 페이지를 변경할 수 있습니다.");
+document.body.innerText = newContent;
+
+// • HTML은 웹 페이지의 구조를 나타냅니다.
+// • CSS는 웹 페이지 안에 있는 요소의 스타일을 나타냅니다.
+// • 자바스크립트는 로직을 통해 문서의 구조와 스타일에 변화를 줍니다.
+// • 브라우저에서는 자바스크립트에서 HTML과 CSS에 접근할 수 있는 API를 제공합니다.
